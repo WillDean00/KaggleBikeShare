ff61571b3b324d23845f7c479cf9b362a8985535
Will Dean <williamrdean00@gmail.com>
2024-09-11 10:04:43 -0600

    After Linear Regression

--- BikeShare.R
+++ BikeShare.R
@@ -1,3 +1,40 @@
-install.packages(tidy)
+install.packages(tidy) 
+install.packages("DataExplorer")
 library(tidyverse)
 library(tidymodels)
+library(vroom)
+
+train <- vroom("train.csv")
+test <- vroom("test.csv")
+vroom(train)
+intro <- plot_intro(train)
+corr <- plot_correlation(train)
+hist <- plot_histogram(train)
+bar <- plot_bar(train)
+miss <- plot_missing(train)
+(corr + hist) / (intro + bar)
+
+library(DataExplorer)
+library(patchwork)
+
+glimpse(train)
+
+
+my_linear_model <- linear_reg() %>% 
+set_engine("lm") %>% 
+set_mode("regression") %>% 
+fit(formula = count ~ temp + humidity, data=train)
+
+
+bike_predictions <- predict(my_linear_model,
+                            new_data=test) 
+bike_predictions
+
+kaggle_submission <- bike_predictions %>%
+bind_cols(., test) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+## Write out the file9
+vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
