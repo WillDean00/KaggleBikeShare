32446eac23b5770bc1a42abd6271dacd279317c6
Will Dean <williamrdean00@gmail.com>
2024-09-13 09:50:47 -0600

    Poisson Update

--- BikeShare.R
+++ BikeShare.R
@@ -38,3 +38,27 @@ bind_cols(., test) %>% #Bind predictions with test data
   mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
 ## Write out the file9
 vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
+
+
+
+
+#poisson regression
+install.packages("poissonreg")
+library(poissonreg)
+
+my_pois_model <- poisson_reg() %>% 
+  set_engine("glm") %>% 
+  set_mode("regression") %>%
+fit(formula=count ~ temp + humidity, data=train)
+bike_predictions <- predict(my_pois_model,
+                            new_data=test) 
+bike_predictions ## Look at the output
+
+
+pois_kaggle_submission <- bike_predictions %>%
+bind_cols(., test) %>% 
+  select(datetime, .pred) %>% 
+  rename(count=.pred) %>% 
+  mutate(datetime=as.character(format(datetime))) 
+
+vroom_write(x=pois_kaggle_submission, file="./PoissonPreds.csv", delim = ",")
