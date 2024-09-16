07ea75e3b4ca88f7cfad7adddb3117236d4d0f82
Will Dean <williamrdean00@gmail.com>
2024-09-16 10:46:01 -0600

    Workflow

--- BikeShare.R
+++ BikeShare.R
@@ -62,3 +62,49 @@ bind_cols(., test) %>%
   mutate(datetime=as.character(format(datetime))) 
 
 vroom_write(x=pois_kaggle_submission, file="./PoissonPreds.csv", delim = ",")
+
+
+
+#Data Wrangling
+new_train <- train %>% 
+  select(-casual, -registered) %>% 
+  mutate(count = log(count)) 
+
+glimpse(new_train)
+view(train)
+glimpse(my_recipe)
+
+my_recipe <- recipe(count ~ ., data = new_train) %>% 
+  step_mutate(weather =ifelse(weather == 4, 3, weather)) %>% 
+  step_mutate(weather = factor(weather, levels = 1:3)) %>% 
+  step_time(datetime, features = c("hour")) %>% 
+  step_mutate(season = factor(season, levels = 1:4, labels = c("spring", "summer", "fall" , "winter"))) %>% 
+  step_rm(datetime, temp, holiday, workingday) %>% 
+  step_mutate(windspeed = ifelse(windspeed == 0, 0, 1))
+
+
+prep_recipe <- prep(my_recipe)
+baked <- bake(prep_recipe, new_data = new_train)
+view(baked)
+
+lin_model <- linear_reg() %>% 
+  set_engine("lm") %>% 
+  set_mode("regression")
+
+bike_workflow <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(lin_model) %>% 
+  fit(data = new_train)
+new_train
+
+lin_preds <- predict(bike_workflow, new_data = test)
+exp_lin_preds <- exp(lin_preds)
+
+
+workflow_kaggle_submission <- exp_lin_preds %>%
+  bind_cols(., test) %>% 
+  select(datetime, .pred) %>% 
+  rename(count=.pred) %>% 
+  mutate(datetime=as.character(format(datetime))) 
+
+vroom_write(x=workflow_kaggle_submission, file=".WorkflowPreds.csv", delim = ",")
